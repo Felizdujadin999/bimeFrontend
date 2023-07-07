0ef9d8f0d82d4a725cfc51e4d179235c43905a6b
james king <jamesasuelimen77@gmail.com>
2023-07-07 15:48:29 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import HomePage from "./pages/dashboard/component/homePage";
+import { Registration } from "./pages/authentication/component/registration";
+import { Login } from "./pages/authentication/component/login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+       <Router>
+        <Routes>
+          <Route path="/" element={<HomePage/>} />
+          <Route path="/signUp" element={<Registration/>} />
+          <Route path = "/login" element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
