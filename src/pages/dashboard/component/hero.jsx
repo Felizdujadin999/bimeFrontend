63a13b39514201e82b77066c78f88641b715f0d0
Felizdujadin <ezeikefelix51@gmail.com>
2023-07-12 10:45:57 +0100

    upload

--- src/pages/dashboard/component/hero.jsx
+++ src/pages/dashboard/component/hero.jsx
@@ -6,13 +6,7 @@ export const Hero =()=>{
 
     return(
         <div className='hero'>
-             <div className={`color-switcher ${state && 'color-switcher--open'}`}>
-                    <button onClick={()=> setState(prevState => !prevState)} className="cpb"><div className="ri-settings-3-fill"></div></button>
-                    <h1 className="heading">personalise</h1>
-                    <div className="color-list">
-                       {colors.map((color, idx)=> <ColorItem  key={idx} setColor={setColor} color = {color}/>)} 
-                    </div>
-                </div>
+            
         </div>
     )
 }
