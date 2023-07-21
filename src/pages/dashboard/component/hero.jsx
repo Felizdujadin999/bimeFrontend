007fcfc05feb2215e833c3827d48faee65d502cd
Felizdujadin <ezeikefelix51@gmail.com>
2023-07-21 08:30:30 +0100

    updated the homepage with cool stuff

--- src/pages/dashboard/component/hero.jsx
+++ src/pages/dashboard/component/hero.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import '../styles/hero.css'
+import slidefour from '../../../assets/images/slides/slibe4.jpg'
+import slidethree from '../../../assets/images/slides/slibe3.jpg'
+import slidetwo from '../../../assets/images/slides/slibe2.jpg'
+import slidefive from '../../../assets/images/slides/slibe5.jpg'
 
 
 export const Hero =()=>{
 
-
-
     return(
         <div className='hero'>
-               
+            <div className="slider">
+                <img className="imgs" src={slidefive} alt="" />
+                <img className="imgs" src={slidefour} alt="" />
+                <img className="imgs" src={slidethree} alt="" />
+                <img className="imgs" src={slidetwo} alt="" />
+            </div>
         </div>
     )
 }
